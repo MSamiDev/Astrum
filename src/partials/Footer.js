748b68555eb17f0f12293472f230d968f72ca5cc
Mohammad Sami <mohammadsami@duck.com>
2022-11-09 21:55:11 +0530

    Remove: footer menu

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -32,7 +32,7 @@ function Footer() {
           </div>
 
           {/* 2nd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+          {/* <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
             <h6 className="text-text font-semibold  mb-2">Products</h6>
             <ul className="text-sm">
               <li className="mb-2">
@@ -51,7 +51,7 @@ function Footer() {
                 <Link to="#" className="text-text hover:text-high transition duration-150 ease-in-out">Command-line</Link>
               </li>                            
             </ul>
-          </div>
+          </div> */}
 
           {/* 3rd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
@@ -98,7 +98,7 @@ function Footer() {
           </div>
 
           {/* 5th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
+          {/* <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
             <h6 className="text-text font-semibold  mb-2">Subscribe</h6>
             <p className="text-sm text-text mb-4">Get the latest news and articles to your inbox every month.</p>
             <form>
@@ -113,13 +113,13 @@ function Footer() {
                         <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                       </svg>
                     </button>
-                  </div>
+                  </div> */}
                   {/* Success message */}
                   {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
-                </div>
+                {/* </div>
               </div>
             </form>
-          </div>          
+          </div>           */}
 
         </div>
 
