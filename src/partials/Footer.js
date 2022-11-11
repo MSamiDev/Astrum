14f3cac5869edf714c9a5a41febf1decd804fdf7
Pranshu321 <pranshujain0111@gmail.com>
2022-11-11 12:27:53 +0530

    Email service added

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+import emailjs from 'emailjs-com';
 
 function Footer() {
+  const [email, setemail] = useState("");
+  function sendMail() {
+    emailjs.send("service_hesknwi", "template_ibzkoaw", { email: email } , 'gP8sKnDLte9gp24k2').then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+      toast.success("Successfully Subscribed");
+      setemail("");
+    }).catch((err)=>{
+      toast.error("Invalid Email or Server Error");
+    })
+  }
   return (
     <footer>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 3000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
         {/* Top area: Blocks */}
@@ -98,28 +135,28 @@ function Footer() {
           </div>
 
           {/* 5th block */}
-          {/* <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
             <h6 className="text-text font-semibold  mb-2">Subscribe</h6>
             <p className="text-sm text-text mb-4">Get the latest news and articles to your inbox every month.</p>
-            <form>
+            <div>
               <div className="flex flex-wrap mb-4">
                 <div className="w-full">
                   <label className="block text-sm sr-only" htmlFor="newsletter">Email</label>
                   <div className="relative flex items-center max-w-xs">
-                    <input id="newsletter" type="email" className="form-input w-full font-semibold px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
-                    <button type="submit" className="absolute inset-0 left-auto" aria-label="Subscribe">
+                    <input id="newsletter" onChange={(e)=>setemail(e.target.value)} type="email" className="form-input w-full font-semibold px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
+                    <button onClick={sendMail} type="submit" className="absolute inset-0 left-auto" aria-label="Subscribe">
                       <span className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
                       <svg className="w-3 h-3 fill-current text-blue-600 mx-3 flex-shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                       </svg>
                     </button>
-                  </div> */}
+                  </div>
                   {/* Success message */}
                   {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
-                {/* </div>
+                </div>
               </div>
-            </form>
-          </div>           */}
+            </div>
+          </div>
 
         </div>
 
